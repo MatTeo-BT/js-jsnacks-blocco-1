e8b4b15beb9085affc984f3e4ccd7870693e2f04
MatTeo-BT <aboutmet@gmail.com>
2023-11-10 11:59:54 +0100

    snack completo

--- snack-1/js/scrpt.js
+++ snack-1/js/scrpt.js
@@ -3,4 +3,9 @@ console.log(etaA);
 const etaB= prompt(`Quanti anni hai?`);
 console.log(etaB);
 
-if
+if (etaA >=0 && etaA <30){
+    console.log(`più piccolo`);
+}
+else if (etaB >=30 && etaB >30){
+    console.log(`più grande`);
+}
