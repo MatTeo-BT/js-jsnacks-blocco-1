6ab8b26bbe1ec558337443ad3d85e5b91392df72
MatTeo-BT <aboutmet@gmail.com>
2023-11-14 12:20:22 +0100

    ultime modifiche

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,5 +1,5 @@
 const lista = document.querySelector (".lista")
 for (let i=0; i<6;i++) {
-    const element = `<li class="text-green"></li>`;
+    const element = `<li class="text-green">ciao</li>`;
     lista.innerHTML += element;
 }
