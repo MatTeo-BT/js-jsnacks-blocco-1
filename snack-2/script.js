d4dcda2e5a8936bfeee0e79295784b136e710404
MatTeo-BT <aboutmet@gmail.com>
2023-11-14 12:19:02 +0100

    correzione ul.innerHTML

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,5 +1,5 @@
 const lista = document.querySelector (".lista")
 for (let i=0; i<6;i++) {
-    const element = <li class="text-green"></li>;
-    ul.innerHTML += element;
+    const element = `<li class="text-green"></li>`;
+    lista.innerHTML += element;
 }
