cad38de1e6fa18665e45dcf0152fc935cddd1512
MatTeo-BT <aboutmet@gmail.com>
2023-11-15 12:25:20 +0100

    snack corretto

---
+++ snack-4/script.js
@@ -0,0 +1,10 @@
+const newArray = [];
+
+for(let i = 1; i <= 100; i++){
+    if(i % 7 === 0){
+        newArray.push(i);
+    } else if( i % 8 === 0){
+        newArray.push(i);
+    }
+}
+console.log(newArray);
