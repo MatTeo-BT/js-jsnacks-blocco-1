c1558fe54fd002817a0f9e19b0b90a1d2863f7a6
MatTeo-BT <aboutmet@gmail.com>
2023-11-16 12:24:37 +0100

    snack completo

---
+++ snack-5/script.js
@@ -0,0 +1,7 @@
+const firstWord = prompt(`Type a word`);
+const secondWord = prompt (`Type another word`);
+if (firstWord > secondWord){
+    console.log(firstWord);
+}else if (firstWord < secondWord){
+    console.log(secondWord)
+}
