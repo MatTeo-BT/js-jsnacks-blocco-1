66b95f8eb5a94ce079f4aca41e566eee617cf0d5
MatTeo-BT <aboutmet@gmail.com>
2023-11-16 16:46:03 +0100

    inserimento array, prompt, ciclo for e if

---
+++ jsnack-7/script.js
@@ -0,0 +1,13 @@
+// Crea un array vuoto. Chiedi per 6 volte all'utente di inserire un numero, se è dispari inseriscilo nell'array.
+const array = [];
+const firstNumber = prompt (`Type a number`)
+const secondNumber = prompt (`Type a number`)
+const thirdNumber = prompt (`Type a number`)
+const fourthNumber = prompt (`Type a number`)
+const fifthNumber = prompt (`Type a number`)
+const sixthNumber = prompt (`Type a number`)
+const odd = /(firstNumber+)(secondNumber+)(thirdNumber+)/;
+for (let i = 0; i < odd; i++)
+if (odd % 2 !=0){
+    console.log(array)
+}
